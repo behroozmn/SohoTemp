75f1b285d9dac57289fb6b9841313682d86cda51
بهروزمحمدی‌نسب-شرکت <behroozmn@chmail.ir>
2025-09-13 19:07:07 +0330

    14040622-19:07

--- pylibs/disk.py
+++ pylibs/disk.py
@@ -1,14 +1,7 @@
 import subprocess  # اجرای دستورات سیستمی در صورت نیاز
 import psutil  # برای خواندن آمار سیستم
 from typing import Dict, Any, Optional, List  # تایپ‌هینت برای خوانایی بهتر
-from django.http import JsonResponse
-import psutil
-from typing import Dict, List, Any, Optional
-import subprocess
-
-import psutil
 import time
-from typing import Dict, Any, Optional, List
 from django.http import JsonResponse
 
 class Disk:
