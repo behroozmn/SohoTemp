a4b6af82764036cfd23f36fdc4f5e330e8378ac3
BehroozMohammadiNasab-SohoServer <behroozmn@chmail.ir>
2025-09-24 17:52:50 +0330

    14040702-17:52

--- soho_core_api/views_collection/view_zpool.py
+++ soho_core_api/views_collection/view_zpool.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from django.http import Http404
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from pylibs.zpool import PoolManager
+from pylibs.pool import PoolManager
 
 
 class ZPoolListView(APIView):
