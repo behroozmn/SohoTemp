a63c33636ca36d628d31cbc4f24a91d2c18a0607
BehroozMohammadiNasab-SohoServer <behroozmn@chmail.ir>
2025-09-27 09:53:29 +0330

    14040705-09:53

--- soho_core_api/urls_collection/url_zpool.py
+++ soho_core_api/urls_collection/url_zpool.py
@@ -5,10 +5,10 @@ from soho_core_api.views_collection import view_zpool
 
 urlpatterns = [
     path("", view_zpool.ZpoolListView.as_view(), name="zpool-list"),
-    path("<str:pool_name>", view_zpool.ZpoolDetailView.as_view(), name="zpool-detail"),
+    path("<str:pool_name>/", view_zpool.ZpoolDetailView.as_view(), name="zpool-detail"),
 
-    path("create", view_zpool.ZpoolCreaView.as_view(), name="create"),
+    path("create", view_zpool.ZpoolCreaView.as_view(), name="zpool-create"),
 
-    path("delete", view_zpool.ZpoolDeleteView.as_view(), name="delete"),
+    path("delete", view_zpool.ZpoolDeleteView.as_view(), name="zpool-delete"),
 
 ]
