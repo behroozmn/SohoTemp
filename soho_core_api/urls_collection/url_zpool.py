3896edb07ffb8f6c00f5462da90440df22be3950
BehroozMohammadiNasab-SohoServer <behroozmn@chmail.ir>
2025-09-27 08:47:48 +0330

    14040705-08:47

--- soho_core_api/urls_collection/url_zpool.py
+++ soho_core_api/urls_collection/url_zpool.py
@@ -5,6 +5,7 @@ from soho_core_api.views_collection import view_zpool
 
 urlpatterns = [
     path("", view_zpool.ZpoolListView.as_view(), name="zpool-list"),
+    path("<str:pool_name>/detail", view_zpool.ZpoolDetailView.as_view(), name="zpool-detail"),
 
     path("create", view_zpool.ZpoolCreaView.as_view(), name="create"),
 
