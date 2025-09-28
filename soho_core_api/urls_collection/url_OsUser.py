a257a31bf9e6b596693a241a0b5e8b01844b884f
BehroozMohammadiNasab-SohoServer <behroozmn@chmail.ir>
2025-09-28 20:13:24 +0330

    14040707-20:13

--- soho_core_api/urls_collection/url_OsUser.py
+++ soho_core_api/urls_collection/url_OsUser.py
@@ -5,5 +5,6 @@ from soho_core_api.views_collection import view_OsUser
 
 urlpatterns = [
     path("", view_OsUser.UserListView.as_view(), name="osuser-list"),
+    path("create/", view_OsUser.UserCreateView.as_view(), name="osuser-create"),
 
 ]
--- soho_core_api/views_collection/view_OsUser.py
+++ soho_core_api/views_collection/view_OsUser.py
@@ -8,12 +8,23 @@ from pylibs.OSUser import UserManager
 
 class UserListView(APIView):
     def get(self, request):
-        include_system: str = request.data.get("include_system","false")
+        include_system: str = request.data.get("include_system", "false")
         manager = UserManager()
         if include_system.lower() in ("true", "1", "yes", "on"):
             result = manager.list_users(include_system=True)
         else:
             result = manager.list_users(include_system=False)
+        if result["ok"]:
+            return Response(result, status=status.HTTP_200_OK)
+        return Response(result, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserCreateView(APIView):
+    def post(self, request):
+        username: str = request.data.get("username")
+        login_shell: str = request.data.get("login_shell")
+        manager = UserManager()
+        result = manager.add_user(username, login_shell)
         if result["ok"]:
             return Response(result, status=status.HTTP_200_OK)
         return Response(result, status=status.HTTP_400_BAD_REQUEST)
