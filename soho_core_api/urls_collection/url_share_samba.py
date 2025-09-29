d6df4341fd0147fc4281ceddc19f5608263ae543
BehroozMohammadiNasab-SohoServer <behroozmn@chmail.ir>
2025-09-29 14:58:20 +0330

    14040707-14:58

--- soho_core_api/urls_collection/url_share_samba.py
+++ soho_core_api/urls_collection/url_share_samba.py
@@ -4,10 +4,10 @@ from soho_core_api.views_collection import view_share_samba
 urlpatterns = [
 
     path('', view_share_samba.SambaListView.as_view()),
-
     path('create/', view_share_samba.SambaCreateView.as_view()),
     path('delete/', view_share_samba.SambaDeleteView.as_view()),
     path('user/add/', view_share_samba.SambaUserCreateView.as_view()),
     path('user/enable/', view_share_samba.SambaUserEnableView.as_view()),
-    path('user/passwd/', view_share_samba.SambaChangePasswordView.as_view()),
+    path('user/passwd/', view_share_samba.SambaUserChangePasswordView.as_view()),
+    path('user/list/', view_share_samba.SambaUserListView.as_view()),
 ]
