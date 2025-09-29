fa9bdaa5764762fdb0eb4e1b95ddbd43a5e4a9a7
BehroozMohammadiNasab-SohoServer <behroozmn@chmail.ir>
2025-09-29 17:17:45 +0330

    14040707-17:17

--- soho_core_api/views_collection/view_os_User.py
+++ soho_core_api/views_collection/view_os_User.py
@@ -8,12 +8,8 @@ from pylibs.OSUser import UserManager
 
 class UserListView(APIView):
     def get(self, request):
-        include_system: str = request.data.get("include_system", "false")
         manager = UserManager()
-        if include_system.lower() in ("true", "1", "yes", "on"):
-            result = manager.list_users(include_system=True)
-        else:
-            result = manager.list_users(include_system=False)
+        result = manager.list_users()
         if result["ok"]:
             return Response(result, status=status.HTTP_200_OK)
         return Response(result, status=status.HTTP_400_BAD_REQUEST)
