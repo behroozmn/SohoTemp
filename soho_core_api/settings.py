259b93fd53ea7505920be3f0b131e83ef6a10d53
بهروزمحمدی‌نسب-شرکت <behroozmn@chmail.ir>
2025-10-08 16:42:46 +0330

    14040716-16:42

--- soho_core_api/settings.py
+++ soho_core_api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f=(i%x1&1309nek6ydpqm%bop#kp2s(fe27smixo)@vyi0(uoh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # behroozMohamadinasab
 
