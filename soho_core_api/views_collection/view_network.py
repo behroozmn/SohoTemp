8582baed5b9638bab657c69625ce95c920c8fce6
بهروزمحمدی‌نسب-شرکت <behroozmn@chmail.ir>
2025-10-13 13:29:03 +0330

    14040721-13:29

--- soho_core_api/views_collection/view_network.py
+++ soho_core_api/views_collection/view_network.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser  # فقط ادمین‌ها
 from rest_framework import status
 
-from pylibs.networkmanager import NetworkManager
+from pylibs.networkManager import NetworkManager
 
 
 @api_view(['GET'])
