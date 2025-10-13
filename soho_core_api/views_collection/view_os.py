e2d72cfa028e9dc38d16a4f7094e5a35109cf40c
بهروزمحمدی‌نسب-شرکت <behroozmn@chmail.ir>
2025-10-13 14:27:05 +0330

    14040721-14:27

--- soho_core_api/views_collection/view_os.py
+++ soho_core_api/views_collection/view_os.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
 from rest_framework import status
-from pylibs.sohoOS import SystemManagements
+from pylibs.sohoSystem import OSManagement
 
 class SystemPowerView(APIView):
     permission_classes = [IsAdminUser]  # فقط ادمین‌ها
@@ -11,14 +11,14 @@ class SystemPowerView(APIView):
         if not action:
             return Response("فیلد 'action' الزامی است (مقادیر مجاز: 'shutdown', 'restart').", status=status.HTTP_400_BAD_REQUEST)
 
-        result = SystemManagements.shutdown_or_restart(action.lower()) # shutdown or restart
+        result = OSManagement.shutdown_or_restart(action.lower()) # shutdown or restart
         status_code = status.HTTP_200_OK if result["ok"] else status.HTTP_400_BAD_REQUEST
         return Response(result, status=status_code)
 
 
 class UserListView(APIView):
     def get(self, request):
-        manager = SystemManagements()
+        manager = OSManagement()
         result = manager.list_users()
         if result["ok"]:
             return Response(result, status=status.HTTP_200_OK)
@@ -29,7 +29,7 @@ class UserCreateView(APIView):
     def post(self, request):
         username: str = request.data.get("username")
         login_shell: str = request.data.get("login_shell")
-        manager = SystemManagements()
+        manager = OSManagement()
         result = manager.add_user(username, login_shell)
         if result["ok"]:
             return Response(result, status=status.HTTP_200_OK)
