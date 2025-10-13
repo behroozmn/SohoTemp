d2b9f9ed07df6b702338bca4caefe3099aa5186f
بهروزمحمدی‌نسب-شرکت <behroozmn@chmail.ir>
2025-10-13 14:38:32 +0330

    14040721-14:38

---
+++ soho_core_api/urls_collection/url_system.py
@@ -0,0 +1,11 @@
+# url_pool.py
+
+from django.urls import path
+from soho_core_api.views_collection import view_systems
+
+urlpatterns = [
+    path("user/", view_systems.UserListView.as_view(), name="osuser-list"),
+    path("user/create/", view_systems.UserCreateView.as_view(), name="osuser-create"),
+    path("power/<str:action>/", view_systems.SystemPowerView.as_view(), name="os-power"),
+
+]
