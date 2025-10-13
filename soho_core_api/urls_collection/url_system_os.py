4a9084577f0eaa14abaecc681be9d01bb8f84fae
BehroozMohammadiNasab-SohoServer <behroozmn@chmail.ir>
2025-10-13 19:04:55 +0330

    14040721-19:04

--- soho_core_api/urls_collection/url_system_os.py
+++ soho_core_api/urls_collection/url_system_os.py
@@ -6,6 +6,9 @@ from soho_core_api.views_collection import view_system_os
 urlpatterns = [
     path("user/", view_system_os.UserListView.as_view(), name="osuser-list"),
     path("user/create/", view_system_os.UserCreateView.as_view(), name="osuser-create"),
+
+    path("user/delete/<str:username>/", view_system_os.UserDeleteView.as_view()),
+
     path("power/<str:action>/", view_system_os.OSpowerView.as_view(), name="os-power"),
 
 ]
