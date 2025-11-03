6b95d164af843cf913bdef228e92b1fdba22fdbb
Soho-Core-Test <behroozmn@chmail.ir>
2025-11-03 16:45:20 +0330

    14040812-16:42-Bug fix blockList JWT

--- soho_core_api/settings.py
+++ soho_core_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'rest_framework',  # behroozMohamadinasab
     'drf_spectacular',  # behroozMohamadinasab
     'rest_framework_simplejwt',  # behroozMohamadinasab
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',  # # behroozMohamadinasab
 ]
 
